a5cd6994f075b9f1a4229aa9ccfde41bab1c314e
Peter-Jones-G1t <peter.jones8@my.jcu.edu.au>
2025-06-02 18:44:50 +1000

    Refactor password checker to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,10 +1,23 @@
 """Password checker"""
 
 MIN_LENGTH = 8
+def main():
+    """Calls other program functions"""
+    password = get_password()
+    print_stars(password)
 
-password = input("Enter password: ")
-while len(password) < MIN_LENGTH:
-    print(f"Password must be at least {MIN_LENGTH} characters long.")
+
+def print_stars(password):
+    """Prints number of stars based on password length"""
+    print('*' * len(password))
+
+
+def get_password():
+    """Gets password and checks it meets length requirements"""
     password = input("Enter password: ")
+    while len(password) < MIN_LENGTH:
+        print(f"Password must be at least {MIN_LENGTH} characters long.")
+        password = input("Enter password: ")
+    return password
 
-print('*' * len(password))
+main()
