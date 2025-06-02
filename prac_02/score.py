14728394632a3635f9cd90cf2dc030f452b2e4f1
Peter-Jones-G1t <peter.jones8@my.jcu.edu.au>
2025-06-02 21:18:05 +1000

    Add randomised score to program

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,13 +2,21 @@
 Program to determine score status
 """
 
+import random
+
+
 def main():
+    """Get score and print result"""
     score = float(input("Enter score: "))
     result = determine_result(score)
     print(f"Your result is {result}")
+    random_score = random.uniform(0, 100)
+    random_result = determine_result(random_score)
+    print(f"Random score: {random_score:.2f} - Result is {random_result}")
 
 
 def determine_result(score):
+    """Determine result based on score"""
     if score < 0 or score > 100:
         return "Invalid score"
     elif score >= 90:
