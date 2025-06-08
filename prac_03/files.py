0a0b82d6cb87af11e539686c5a943c2a9c9aa2a2
Peter-Jones-G1t <peter.jones8@my.jcu.edu.au>
2025-06-08 22:17:23 +1000

    Add program to read name in .txt file then print name

--- prac_03/files.py
+++ prac_03/files.py
@@ -8,3 +8,8 @@ user_name = input("Enter you name: ")
 name_file = open(FILENAME, 'w')
 print(user_name, file=name_file)
 name_file.close()
+
+name_file = open(FILENAME, 'r')
+read_file = name_file.readline().strip()
+print(f"Hi {read_file}!")
+name_file.close()
