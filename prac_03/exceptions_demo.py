42f5acda820006504759ed32090049d96184de81
Peter-Jones-G1t <peter.jones8@my.jcu.edu.au>
2025-06-08 20:18:19 +1000

    Add comments to answer questions and provides code to replace the ZeroDivisionError except suite

---
+++ prac_03/exceptions_demo.py
@@ -0,0 +1,23 @@
+"""
+CP1404/CP5632 - Practical
+Answer the following questions:
+1. When will a ValueError occur?
+2. When will a ZeroDivisionError occur?
+3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+"""
+
+try:
+    numerator = int(input("Enter the numerator: "))
+    denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print("Denominator cannot be zero.")
+        denominator = int(input("Enter the denominator: "))
+    fraction = numerator / denominator
+    print(fraction)
+except ValueError:
+    print("Numerator and denominator must be valid numbers!")
+print("Finished.")
+
+#1. ValueError will occur when anything other than an integer is entered e.g letters or special characters
+#2. ZeroDivisionError will occur when a 0 is entered into the denominator input (trying to divide by 0)
+#3. yes you could by using the LBYL approach
