5f2f1c5a22812aafd8216e30d92500a08f5944f7
Peter-Jones-G1t <peter.jones8@my.jcu.edu.au>
2025-06-08 22:36:58 +1000

    Add program to read first 2 lines and print total

--- prac_03/files.py
+++ prac_03/files.py
@@ -4,12 +4,21 @@ Program to practice reading and writing to files
 
 FILENAME = "name.txt"
 
+# program 1.
 user_name = input("Enter you name: ")
 name_file = open(FILENAME, 'w')
 print(user_name, file=name_file)
 name_file.close()
 
+# program 2.
 name_file = open(FILENAME, 'r')
-read_file = name_file.readline().strip()
+read_file = name_file.read().strip()
 print(f"Hi {read_file}!")
 name_file.close()
+
+# program 3.
+with open("numbers.txt", 'r') as numbers_file:
+    total = int(numbers_file.readline()) + int(numbers_file.readline())
+    print(total)
+
+
