22b358501dd8b31f3c87af3725331f47778fa4b4
Peter-Jones-G1t <peter.jones8@my.jcu.edu.au>
2025-06-14 18:44:59 +1000

    Add slice to print all elements of list except first two. Add a check for the number 9 in list.

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -1,5 +1,5 @@
 """
-List warmup activity
+CP1404 list warmup activity
 """
 
 numbers = [3, 1, 4, 1, 5, 9, 2]
@@ -13,7 +13,17 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # "3" in numbers = False
 # numbers + [6, 5, 3] = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 
+# 1. changes first element in list to "ten"
 numbers[0] = "ten"
+
+# 2. changes last element in list to 1
 numbers[-1] = 1
 
-print(numbers)
+# 3. prints all elements in the list except the first two
+print(numbers[2:])
+
+# 4. checks and prints if 9 is an element of the list
+if 9 in numbers:
+    print("9 is in the list")
+else:
+    print("9 is not in the list")
