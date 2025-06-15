43f8ac2a1cf54e7252c61e74976c22ec835271ea
Peter-Jones-G1t <peter.jones8@my.jcu.edu.au>
2025-06-15 17:43:03 +1000

    Add code to call a function based on integer input from user. Add function that generates a quick pick checking. Add code to print quick picks generated

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,6 +1,7 @@
 """
 CP1404 Quick pick generator
 """
+import random
 
 NUMBERS_PER_PICK = 6
 MIN_NUMBER = 1
@@ -8,8 +9,23 @@ MAX_NUMBER = 45
 
 
 def main():
+    """Get number of picks from user then prints picks"""
     number_of_picks = int(input("How many quick picks? "))
-    print(f"{number_of_picks} picks to generate")
+
+    for _ in range(number_of_picks):
+        quick_pick = generate_quick_pick()
+        print(" ".join(f"{number:2}" for number in quick_pick))
+
+
+def generate_quick_pick():
+    """Generate a single quick pick checking that no two numbers are the same"""
+    numbers = []
+    while len(numbers) < NUMBERS_PER_PICK:
+        number = random.randint(MIN_NUMBER, MAX_NUMBER)
+        if number not in numbers:
+            numbers.append(number)
+    numbers.sort()
+    return numbers
 
 
 main()
