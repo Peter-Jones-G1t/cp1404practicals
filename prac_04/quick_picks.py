2eb2c6b0cc6eccf0384f5dee703691dced1e239a
Peter-Jones-G1t <peter.jones8@my.jcu.edu.au>
2025-06-15 17:18:00 +1000

    Add quick_picks.py to prac_04. Add starter code including: constants, and input prompt

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,15 @@
+"""
+CP1404 Quick pick generator
+"""
+
+NUMBERS_PER_PICK = 6
+MIN_NUMBER = 1
+MAX_NUMBER = 45
+
+
+def main():
+    number_of_picks = int(input("How many quick picks? "))
+    print(f"{number_of_picks} picks to generate")
+
+
+main()
