46fec60f6bc5431d7a4af51d55956234d6455a63
Peter-Jones-G1t <peter.jones8@my.jcu.edu.au>
2025-06-22 13:31:43 +1000

    Complete prac 5 hex colours activity

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,15 @@
+"""
+Program to print hex code of chosen colour
+"""
+
+COLOURS_TO_HEX = {"Rifle Green": "#444c38", "Inchworm": "#b2ec5d", "Copper": "#b87333", "Kelly Green": "#4cbb17",
+                  "Rose": "#ff007f", "Old Gold": "#cfb53b", "Lilac": "#c8a2c8", "Linen": "#faf0e6",
+                  "Eggplant": "#614051", "Denim": "#1560bd"}
+
+colour_name = input("Enter a colour: ").title()
+while colour_name != "":
+    try:
+        print(f"{colour_name}'s hex code is {COLOURS_TO_HEX[colour_name]}")
+    except KeyError:
+        print("Invalid colour")
+    colour_name = input("Enter a colour: ").title()
