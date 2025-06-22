f73a8bff462a3ad322331176071eebb9dbfff61b
Peter-Jones-G1t <peter.jones8@my.jcu.edu.au>
2025-06-22 13:48:54 +1000

    Refactor dictionary name to follow style guide

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,14 +2,14 @@
 Program to print hex code of chosen colour
 """
 
-COLOURS_TO_HEX = {"Rifle Green": "#444c38", "Inchworm": "#b2ec5d", "Copper": "#b87333", "Kelly Green": "#4cbb17",
+COLOUR_TO_HEX = {"Rifle Green": "#444c38", "Inchworm": "#b2ec5d", "Copper": "#b87333", "Kelly Green": "#4cbb17",
                   "Rose": "#ff007f", "Old Gold": "#cfb53b", "Lilac": "#c8a2c8", "Linen": "#faf0e6",
                   "Eggplant": "#614051", "Denim": "#1560bd"}
 
 colour_name = input("Enter a colour: ").title()
 while colour_name != "":
     try:
-        print(f"{colour_name}'s hex code is {COLOURS_TO_HEX[colour_name]}")
+        print(f"{colour_name}'s hex code is {COLOUR_TO_HEX[colour_name]}")
     except KeyError:
         print("Invalid colour")
     colour_name = input("Enter a colour: ").title()
