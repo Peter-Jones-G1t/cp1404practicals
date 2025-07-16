16dc5f8de35730ad0d176708c891608fca2f5d7e
Peter-Jones-G1t <peter.jones8@my.jcu.edu.au>
2025-07-16 23:45:53 +1000

    Add completed code to prac_06 and refactor code to use functions

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -1,38 +1,38 @@
 """
 Program to store all your guitars!
 Estimate: 30 mins
-Actual:
+Actual: 45 mins
 """
 
 from prac_06.guitar import Guitar
 
 
 def main():
+    """Stores and prints a list of your guitars"""
     print("My guitars!")
 
     guitars = []
-
-    # name = input("Name: ")
-    # while name != "":
-    #     year = int(input("Year: "))
-    #     cost = int(input("Cost: "))
-
-    # guitar = Guitar(name, year, cost)
-    # guitars.append(guitar)
-
-    # print(f"{guitar} added.\n")
-
-    # name = input("Name: ")
-
-    guitars.append(Guitar("Fender Stratocaster", 2014, 765.40))
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
-
+    get_guitar(guitars)
 
     print("\nThese are my guitars:")
     for i, guitar in enumerate(guitars, 1):
-        vintage_string = " (vintage)" if guitar.is_vintage() else ""
+        vintage_string = " (vintage)" if guitar.is_vintage() else "" # check if guitar in list is vintage
         print(f"Guitar {i}: {guitar.name:>20} ({guitar.year}), worth {guitar.cost:10,.2f}{vintage_string}")
+
+
+def get_guitar(guitars):
+    """Get a valid guitar"""
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = int(input("Cost: "))
+
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+
+        print(f"{guitar} added.\n")
+
+        name = input("Name: ")
 
 
 main()
