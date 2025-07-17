83e1aa7d35067864a4dd5e4bb1e6101db04fa201
Peter-Jones-G1t <peter.jones8@my.jcu.edu.au>
2025-07-18 02:50:41 +1000

    Add starter code for Project Management Program exercise

---
+++ prac_07/project.py
@@ -0,0 +1,13 @@
+"""
+Class file for Project Management Program
+"""
+
+
+class Project:
+
+    def __init__(self, name, date, priority, cost, percentage):
+        self.name = name
+        self.date = date
+        self.priority = priority
+        self.cost = cost
+        self.percentage = percentage
---
+++ prac_07/project_management.py
@@ -0,0 +1,20 @@
+"""
+Do-from-scratch Exercise - Project Management Program
+Estimate: 130 mins
+Actual:
+"""
+
+MENU = """- (L)oad projects
+- (S)ave projects
+- (D)isplay projects
+- (F)ilter projects by date
+- (A)dd new project
+- (U)pdate project
+- (Q)uit"""
+
+
+
+def main():
+    print("Welcome to Pythonic Project Management")
+    print(MENU)
+main()
