4b359cc5ac7a6eb4e9113e82479a0d1f4bb1c462
Peter-Jones-G1t <peter.jones8@my.jcu.edu.au>
2025-07-18 04:00:57 +1000

    Create function to load from a csv with error checking and add is_complete and __str__ methods to class file

--- prac_07/project.py
+++ prac_07/project.py
@@ -5,9 +5,18 @@ Class file for Project Management Program
 
 class Project:
 
-    def __init__(self, name, date, priority, cost, percentage):
+    def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
         self.name = name
-        self.date = date
+        self.start_date = start_date
         self.priority = priority
-        self.cost = cost
-        self.percentage = percentage
+        self.cost_estimate = cost_estimate
+        self.completion_percentage = completion_percentage
+
+    def is_complete(self):
+        return self.completion_percentage == 100
+
+    def __str__(self):
+        return (f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate:.2f},"
+                f" completion: {self.completion_percentage}%")
+
+
--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -3,6 +3,9 @@ Do-from-scratch Exercise - Project Management Program
 Estimate: 130 mins
 Actual:
 """
+from prac_07.project import Project
+
+DEFAULT_FILENAME = "projects.txt"
 
 MENU = """- (L)oad projects
 - (S)ave projects
@@ -13,8 +16,39 @@ MENU = """- (L)oad projects
 - (Q)uit"""
 
 
-
 def main():
     print("Welcome to Pythonic Project Management")
+    projects = load_projects(DEFAULT_FILENAME)
+    # test code to see the loaded list
+    for project in projects:
+        print(project)
     print(MENU)
+
+
+
+def load_projects(filename):
+    """Load projects from an entered filename"""
+    projects = []
+    try:
+        with open(filename, 'r') as in_file:
+            in_file.readline()
+            for line in in_file:
+                parts = line.strip().split('\t')
+                name = parts[0]
+                start_date = parts[1]
+                priority = parts[2]
+                cost_estimate = parts[3]
+                completion_percentage = parts[4]
+                project = Project(name, start_date, int(priority), float(cost_estimate), int(completion_percentage))
+                projects.append(project)
+        print(f"{len(projects)} projects loaded from {filename}")
+    except FileNotFoundError:
+        print(f"File '{filename}' not found.")
+    return projects
+
+
+
+
+
+
 main()
